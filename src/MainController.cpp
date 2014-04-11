320df1d3e4c658659bd265b8f5669e261ba2ea10
Nicolas Brignol <tools@octet-studio.com>
2014-04-11 10:56:24 +0200

    Game loop : sleep when wait.

--- src/MainController.cpp
+++ src/MainController.cpp
@@ -1,5 +1,6 @@
 #include "MainController.h"
 #include <ctime>
+#include <unistd.h>
 
 MainController::MainController(){
    this->running = false;
@@ -12,26 +13,30 @@ void MainController::run(){
 	this->running = true;
 
   int lastStepCallMs = clock() / (CLOCKS_PER_SEC / 1000);
-  int expectedDuration = 1000/3;
+  int expectedDuration = 1000/60;
+  int totalSleepMs = 0;
 
   while(this->running) {
-    int nowMs = clock() / (CLOCKS_PER_SEC / 1000);
-    int diffMs = nowMs - lastStepCallMs;
+    int nowMs = totalSleepMs + clock() / (CLOCKS_PER_SEC / 1000);
+    int effectiveMs = nowMs - lastStepCallMs;
+    int diffMs = expectedDuration - effectiveMs;
 
     std::cout << "Now " << nowMs << " ms" << std::endl;
     std::cout << "Last " << lastStepCallMs << " ms" << std::endl;
-    std::cout << "Diff " << diffMs << " ms" << std::endl;
+    std::cout << "Effective " << effectiveMs << " ms" << std::endl;
     std::cout << "Expected  " << expectedDuration << " ms" << std::endl;
+    std::cout << "Diff  " << diffMs << " ms" << std::endl;
 
-    if (diffMs < expectedDuration) {
-      std::cout << "[Wait] " ;
-      //wait
+    if (effectiveMs < expectedDuration) {
+      std::cout << "[Wait: " << diffMs << " ms ] " << std::endl;
+      usleep(diffMs * 1000);
+      totalSleepMs += diffMs;
       continue;
     }
 
     std::cout << "[Work] " << std::endl;
     this->step();
-    lastStepCallMs = clock() / (CLOCKS_PER_SEC / 1000);
+    lastStepCallMs =  totalSleepMs + clock() / (CLOCKS_PER_SEC / 1000);
   }
     
   this->renderer->stop();
