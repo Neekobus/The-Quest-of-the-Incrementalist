c19dbdd7dde371b5231cc693d95931a3c34d0227
Nicolas Brignol <tools@octet-studio.com>
2014-04-11 10:43:27 +0200

    Execute STEP every 0.3s.

--- src/MainController.cpp
+++ src/MainController.cpp
@@ -1,4 +1,5 @@
 #include "MainController.h"
+#include <ctime>
 
 MainController::MainController(){
    this->running = false;
@@ -10,17 +11,36 @@ void MainController::run(){
 	this->renderer->start();
 	this->running = true;
 
-    while(this->running) {
-        this->step();
+  int lastStepCallMs = clock() / (CLOCKS_PER_SEC / 1000);
+  int expectedDuration = 1000/3;
+
+  while(this->running) {
+    int nowMs = clock() / (CLOCKS_PER_SEC / 1000);
+    int diffMs = nowMs - lastStepCallMs;
+
+    std::cout << "Now " << nowMs << " ms" << std::endl;
+    std::cout << "Last " << lastStepCallMs << " ms" << std::endl;
+    std::cout << "Diff " << diffMs << " ms" << std::endl;
+    std::cout << "Expected  " << expectedDuration << " ms" << std::endl;
+
+    if (diffMs < expectedDuration) {
+      std::cout << "[Wait] " ;
+      //wait
+      continue;
     }
+
+    std::cout << "[Work] " << std::endl;
+    this->step();
+    lastStepCallMs = clock() / (CLOCKS_PER_SEC / 1000);
+  }
     
-    this->renderer->stop();
+  this->renderer->stop();
 }
 
 void MainController::step(){
 	this->actor->x = this->actor->x + 1;
-    this->renderer->showActor(this->actor);
-    this->running = this->renderer->waitForExit(); 
+  this->renderer->showActor(this->actor);
+  this->running = this->renderer->waitForExit(); 
 }
 
 MainController::~MainController(){
